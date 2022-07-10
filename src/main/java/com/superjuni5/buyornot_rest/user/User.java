92ed3676f62897d96f9664a904c2620450729d6c
HanaHww2 <hanahww2@gmail.com>
2022-07-10 12:19:10 +0900

    feat: initial commit

---
+++ src/main/java/com/superjuni5/buyornot_rest/user/User.java
@@ -0,0 +1,25 @@
+package com.superjuni5.buyornot_rest.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Data @AllArgsConstructor
+@JsonIgnoreProperties(value={"password", "ssn"})
+public class User {
+    private Integer id;
+
+    @Size(min=2, message = "Name은 두 글자 이상 입력해주세요.")
+    private String name;
+
+    @Past
+    private Date joinDate;
+
+//    @JsonIgnore
+    private String password;
+//    @JsonIgnore
+    private String ssn;
+}
---
+++ src/test/java/com/superjuni5/buyornot_rest/BuyOrNotRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superjuni5.buyornot_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyOrNotRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
